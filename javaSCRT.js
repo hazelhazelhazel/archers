d9678e73d3dcd0cf6678edf1b1eceee98fcdc36a
hazelhazelhazel <98513698+hazelhazelhazel@users.noreply.github.com>
2022-10-14 01:39:16 +0800

    Add files via upload

---
+++ javaSCRT.js
@@ -0,0 +1,18 @@
+// Function to upload article
+function uploadArticle() 
+{
+  alert("Your file has successfully been uploaded. You may view it in the My Articles Tab."); // creates a pop up notifying admin that file is uploaded with a link going to myarticles.html
+  window.location.href="myarticles.html"; //a link going to myarticles.html 
+  return false;
+}
+
+function login()
+{
+  alert("Login Successful");
+  window.location.href="main.html";
+  return false;
+}
+
+
+
+
